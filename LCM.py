29c6b40c7a3fa0cad3dbcd1fa7f4468ec90e468d
ffekirnew <ffekirnew0808@gmail.com>
2022-07-16 01:41:40 +0300

    lab 1 files

--- LCM.py
+++
@@ -1,4 +0,0 @@
-from EuclidsGcd import GCD
-
-def LCM(a, b):
-	return (a * b // GCD(a, b))
--- SumDigits.py
+++
@@ -1,6 +0,0 @@
-def sum_digits(int):
-	if int < 10: return int
-	else: return int % 10 + sum_digits(int // 10)
-
-
-# manchester united really has to be like one of the world's best clubs because they have b een nothing but the best for as long as i can remember and then some Indian guys like to talk to American people because they feel like they are their masters specially when it comes to movie making and then some.
